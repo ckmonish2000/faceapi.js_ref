204379b913073127f0cf6a5a7b4569655c18d408
monish karunakaran <ckmonish2000@gmail.com>
2021-10-12 21:05:03 +0530

    detect faces

--- script.js
+++ script.js
@@ -1,8 +1,35 @@
 const video = document.querySelector('.videos');
-console.log(video)
+
+
+async function loadModels() {
+  const MODEL_URL = '/models' //model directory
+  await faceapi.nets.loadSsdMobilenetv1Model.loadFromUri(MODEL_URL)
+  await faceapi.nets.loadFaceLandmarkModel.loadFromUri(MODEL_URL) // model to detect face landmark
+  await faceapi.nets.loadFaceRecognitionModel.loadFromUri(MODEL_URL) //model to Recognise Face
+  await faceapi.nets.loadFaceExpressionModel.loadFromUri(MODEL_URL) //model to detect face expression
+
+}
+
+
+loadModels()
 
 navigator.getUserMedia(
   { video: {} },
-  stream => { video.srcObject = stream },
+  async (stream) => {
+    video.srcObject = stream
+  },
   error => { console.log(error) }
 )
+
+
+
+video.addEventListener('play', () => {
+  setInterval(async () => {
+    const video = document.querySelector('.videos');
+    let faceDescriptions = await faceapi.detectAllFaces(video)
+      .withFaceLandmarks()
+      .withFaceDescriptors()
+      .withFaceExpressions()
+    console.log(faceDescriptions)
+  }, 100)
+})
